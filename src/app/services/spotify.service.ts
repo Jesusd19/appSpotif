0485b02f0639aa25221239c3970293b31908f197
Yenkis <jesusdbarajas@gmail.com>
2019-02-01 15:07:48 -0500

    centralizando las peticones a la API

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -12,27 +12,45 @@ export class SpotifyService {
   TOKEN_SPOTIFY: string;
 
   constructor(private http: HttpClient) {
-    this.TOKEN_SPOTIFY = 'BQD3Bw3eL5nAKcg65i0bvnrxhTf2W2p3A5cVNX33SmBPGCt1Y3E91CIwWwzp4mHGIOUvM8OM9sO3-HAP_QE';
+    this.TOKEN_SPOTIFY = 'BQDemuIWo2yVwtT0rlU2MStqCby7vKN43x1MSJ1ozJDPkWbL0_dv3-QA5yVHy8A89f-wm8yfzdHo1kWMz6Y';
     console.log("Spotify services listo");
   }
 
-  getNewReleases() {
-    // Este token hay que generarlo para que la app funcione
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
     const headers = new HttpHeaders({
       'Authorization': `Bearer ${this.TOKEN_SPOTIFY}`
     })
-    // Si queremos limitar agremamos ?limit=20
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
+
+    return this.http.get(url, { headers });
+  }
+
+  getNewReleases() {
+
+    return this.getQuery('browse/new-releases?limit=20')
       .pipe(map(data => data['albums'].items));
+
+    // Este token hay que generarlo para que la app funcione
+    // const headers = new HttpHeaders({
+    //   'Authorization': `Bearer ${this.TOKEN_SPOTIFY}`
+    // })
+    // Si queremos limitar agremamos ?limit=20
+
+    // return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
+    // .pipe(map(data => data['albums'].items));
   }
 
   getArtist(term: string) {
-    // Este token hay que generarlo para que la app funcione
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${this.TOKEN_SPOTIFY}`
-    })
-    // Buscar tipo artista  <
-    return this.http.get(`https://api.spotify.com/v1/search?q=${term}&type=artist&limit=15`, { headers })
+
+    return this.getQuery(`search?q=${term}&type=artist&limit=15`)
       .pipe(map(data => data['artists'].items));
+
+    // Este token hay que generarlo para que la app funcione
+    // const headers = new HttpHeaders({
+    //   'Authorization': `Bearer ${this.TOKEN_SPOTIFY}`
+    // })
+    // Buscar tipo artista  <
+    // return this.http.get(`https://api.spotify.com/v1/search?q=${term}&type=artist&limit=15`, { headers })
+    //   .pipe(map(data => data['artists'].items));
   }
 }
